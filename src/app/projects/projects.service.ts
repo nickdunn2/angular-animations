fb83f91399702835bfd510742f3bd0b1d33ea841
nickdunn2 <nick.dunn@flosports.tv>
2017-12-16 21:10:57 -0600

    styling improvements

--- src/app/projects/projects.service.ts
+++ src/app/projects/projects.service.ts
@@ -15,7 +15,7 @@ export class ProjectsService {
     const prjLoader = Observable.create((observer: Observer<Project[]>) => {
       setTimeout(() => {
         observer.next(this.projects);
-      }, 2000);
+      }, 500);
     });
     return prjLoader;
   }
