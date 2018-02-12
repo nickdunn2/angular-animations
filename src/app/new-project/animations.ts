bcfb3b89dde90f7303b5fa64a3bf00d48eab7156
nickdunn2 <nick.dunn@flosports.tv>
2018-02-12 14:12:29 -0600

    feat: add input validation animation

--- src/app/new-project/animations.ts
+++ src/app/new-project/animations.ts
@@ -1,14 +1,14 @@
-import { animate, group, state, style, transition, trigger } from '@angular/animations'
+import { animate, group, query, state, style, transition, trigger } from '@angular/animations'
 
 export const buttonStateTrigger = trigger('buttonState', [
-  state('valid', style({
-    backgroundColor: 'green',
-    borderColor: 'darkgreen'
-  })),
-  state('invalid', style({
-    backgroundColor: 'lightgray',
-    borderColor: 'gray'
-  })),
+  // state('valid', style({
+  //   backgroundColor: 'green',
+  //   borderColor: 'darkgreen'
+  // })),
+  // state('invalid', style({
+  //   backgroundColor: 'lightgray',
+  //   borderColor: 'gray'
+  // })),
   transition('invalid => valid', [
     group([
       animate(100, style({
@@ -34,5 +34,16 @@ export const buttonStateTrigger = trigger('buttonState', [
     animate(200, style({
       transform: 'scale(1)'
     }))
+  ])
+])
+
+export const formStateTrigger = trigger('formState', [
+  transition('* => *', [
+    query('input.ng-invalid:focus', [
+      animate(200, style({
+        backgroundColor: 'red'
+      })),
+      animate(200)
+    ], { optional: true })
   ])
 ])
